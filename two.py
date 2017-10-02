6e00b0845a7ca1b7ec36de495e2c203bdaa3ce26
heavenborn <alpegno@gmail.com>
2017-10-01 22:36:58 -0400

    Update two.py

--- two.py
+++ two.py
@@ -1,16 +1,47 @@
-from one import *
-import matplotlib.pyplot as plt
+from scipy import optimize
+
+import one
 
 
+class trainer(object):
+    def __init__(self, N):
+        # Make Local reference to network:
+        self.N = N
+
+    def callbackF(self, params):
+        self.N.setParams(params)
+        self.P.append(self.N.costFunction(self.X, self.y))
+
+    def performanceFunctionWrapper(self, params, X, y):
+        self.N.setParams(params)
+        perf = self.N.performanceFunction(X, y)
+        grad = self.N.computeGradients(X, y)
+
+        return perf, grad
+
+    def train(self, x, y):
+        # Make an internal variable for the callback function:
+        self.X = x
+        self.y = y
+
+        # Make empty list to store costs:
+        self.P = []
+
+        params0 = self.N.getParams()
+
+        options = {'maxiter': 200, 'disp': True}
+        _res = optimize.minimize(self.performanceFunctionWrapper, params0, jac=True, method='BFGS',
+                                 args=(x, y), options=options, callback=self.callbackF)
+
+        self.N.setParams(_res.x)
+        self.optimizationResults = _res
 
 
-X = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
+x = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
 y = np.array(([75], [82], [93]), dtype=float)
 
-
-
 NN = Neural_Network()
-yHat = NN.forward(X)
-print(yHat)
 
-djdW1, djdW2 = NN.costFunctionPrime(X, y)
+T = trainer(NN)
+T.train(x, y)
+
